061a03c06535471b5f52edf4db55271bc648a2d5
ShuwenHe <1201220707@pku.edu.cn>
2022-02-07 14:18:57 +0800

    feat: sidsa left join

--- left-join.sql
+++ left-join.sql
@@ -18,3 +18,7 @@ LEFT JOIN
     emp e 
 ON
     d.DEPTNO = e.DEPTNO ;
+
+-- sidsa-service
+-- 
+SELECT count(*) FROM `sid_album` LEFT JOIN sid_photo ON sid_photo.album_id = sid_album.id GROUP BY `sid_album`.`id` ORDER BY sid_album.photo_updated_at desc,sid_album.id desc;
