be81b742b9c967ae6a11af2f2d2a99d9b3c7db24
ShuwenHe <1201220707@pku.edu.cn>
2022-02-08 10:16:35 +0800

    feat: delete sidsa

---
+++ delete.sql
@@ -0,0 +1,3 @@
+-- sidsa-admin
+-- /Users/shareit/Projects/sidsa/sidsa-admin/app/sidsa/service/sid_photo.go:102
+DELETE FROM `sid_photo` WHERE `sid_photo`.`id` = 107
--- select.sql
+++ select.sql
@@ -1,6 +1,5 @@
-select *
-from users;
-
-select username
-from bookstore.users
-where username = "admin4"
+SELECT count(*) FROM `iot_user_project` WHERE user_id = 435 AND project_id = 290319 AND `iot_user_project`.`deleted_at` IS NULL;
+SELECT count(*) FROM `iot_user_project` WHERE user_id = 434 AND project_id = 190304 AND `iot_user_project`.`deleted_at` IS NULL;
+-- sidsa-admin
+-- /Users/shareit/Projects/sidsa/sidsa-admin/app/sidsa/service/sid_photo.go:31
+SELECT sid_photo.*, DATE_FORMAT(created_at,'%Y-%m-%d') as date FROM `sid_photo` ORDER BY created_at desc LIMIT 1000;
