2b945751c4d5085b33e9e2d0b5f1a519b074fce7
ShuwenHe <1201220707@pku.edu.cn>
2022-02-08 11:08:45 +0800

    feat: sidsa-admin

--- select.sql
+++ select.sql
@@ -3,3 +3,5 @@ SELECT count(*) FROM `iot_user_project` WHERE user_id = 434 AND project_id = 190
 -- sidsa-admin
 -- /Users/shareit/Projects/sidsa/sidsa-admin/app/sidsa/service/sid_photo.go:31
 SELECT sid_photo.*, DATE_FORMAT(created_at,'%Y-%m-%d') as date FROM `sid_photo` ORDER BY created_at desc LIMIT 1000;
+-- /Users/shareit/Projects/sidsa/sidsa-admin/app/sidsa/service/sid_photo.go:33
+SELECT count(*) FROM `sid_photo`;
