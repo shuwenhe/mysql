0fd36e1f820ab26be03ccc58f9f744ccd4c7b765
何书文（HSW01） <HSW01@SID.CN>
2022-02-08 11:19:31 +0800

    fix: del

--- 4.Delete/delete-ms-part.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM "public"."banners" WHERE "id" = '15a2915c1f934ebd8ac480fa433'
-DELETE FROM "banners"  WHERE (id = '7c9b613891f54479b6ab45170cdf181e') 
-DELETE FROM "public"."app_upgrades" WHERE "id"='292' OR "id"='293' OR "id"='294';
--- delete.sql
+++ delete.sql
@@ -1,3 +1,8 @@
 -- sidsa-admin
 -- /Users/shareit/Projects/sidsa/sidsa-admin/app/sidsa/service/sid_photo.go:102
 DELETE FROM `sid_photo` WHERE `sid_photo`.`id` = 107
+
+DELETE FROM `users`  WHERE (name='qimi2') 
+DELETE FROM "public"."banners" WHERE "id" = '15a2915c1f934ebd8ac480fa433'
+DELETE FROM "banners"  WHERE (id = '7c9b613891f54479b6ab45170cdf181e') 
+DELETE FROM "public"."app_upgrades" WHERE "id"='292' OR "id"='293' OR "id"='294';
