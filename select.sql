da1f51ef2e1b798a6a33937a154821794fe13e4e
何书文（HSW01） <HSW01@SID.CN>
2022-02-16 16:30:08 +0800

    feat: select

--- select.sql
+++ select.sql
@@ -5,3 +5,5 @@ SELECT count(*) FROM `iot_user_project` WHERE user_id = 434 AND project_id = 190
 SELECT sid_photo.*, DATE_FORMAT(created_at,'%Y-%m-%d') as date FROM `sid_photo` ORDER BY created_at desc LIMIT 1000;
 -- /Users/shareit/Projects/sidsa/sidsa-admin/app/sidsa/service/sid_photo.go:33
 SELECT count(*) FROM `sid_photo`;
+-- GET \"/albums/13/photos\"
+SELECT sid_photo.*, DATE_FORMAT(created_at,'%Y-%m-%d') as date FROM sidsa_service.`sid_photo` WHERE album_id = 13 ORDER BY created_at desc LIMIT 30;
