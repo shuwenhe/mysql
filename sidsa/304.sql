2cf3c56f386ee7f390a1dd65d4ddcf30d516c6df
ShuwenHe <1201220707@pku.edu.cn>
2022-03-03 16:58:28 +0800

    fix: sidsa

--- sidsa/304.sql
+++ sidsa/304.sql
@@ -3,6 +3,6 @@ SELECT * FROM sidsa_service.iot_variant where imei = 240305005054406;
 SELECT * FROM sidsa_service.iot_variant iv WHERE name LIKE '%时间%' HAVING imei = 240305005054406;
 -- 破碎机S 100-199
 -- 100
-SELECT * FROM sidsa_service.iot_variant where name = "破碎机液压站运行时间";
+SELECT * FROM sidsa_service.iot_variant where name = "破碎机液压站运行时间" AND imei = 240305005054406;
 -- 101
 SELECT * FROM sidsa_service.iot_variant where name = "破碎循环运行时间";
