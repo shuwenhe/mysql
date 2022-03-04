eb46368b9f3f7af3581455f60caeaa680bd8fac5
何书文（HSW01） <HSW01@SID.CN>
2022-03-04 13:19:35 +0800

    fix: 304

--- sidsa/304.sql
+++ sidsa/304.sql
@@ -1,4 +1,5 @@
 -- PR90304张家港华瑞
+-- IMEI 240305005054406
 SELECT * FROM sidsa_service.iot_variant where imei = 240305005054406;
 SELECT * FROM sidsa_service.iot_variant iv WHERE name LIKE '%时间%' HAVING imei = 240305005054406;
 -- 破碎机S 100-199
