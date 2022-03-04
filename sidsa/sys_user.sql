1b32b419d6f027a6d0d62ca756475e1437adbb74
何书文（HSW01） <HSW01@SID.CN>
2022-03-04 10:10:17 +0800

    fix: sys_user

--- sidsa/sys_user.sql
+++ sidsa/sys_user.sql
@@ -1,1 +1,4 @@
 SELECT * FROM  sidsa_service.sys_user su;
+SELECT * FROM  sidsa_service.sys_user su WHERE nick_name  ='郑玉晶';
+SELECT * FROM  sidsa_service.sys_user su  WHERE role_id  = '4';
+SELECT * FROM  sidsa_service.sys_user su WHERE phone = '18513182830';
