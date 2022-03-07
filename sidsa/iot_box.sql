7c9f90f8abeb7057997bd48274f79730d1b64d1c
何书文（HSW01） <HSW01@SID.CN>
2022-03-07 11:37:03 +0800

    fix: iot box

--- sidsa/iot_box.sql
+++ sidsa/iot_box.sql
@@ -1,1 +1,4 @@
 SELECT * FROM sidsa_service.iot_box ib;
+SELECT * FROM sidsa_service.iot_box ib WHERE emp_id ='18';
+SELECT * FROM sidsa_service.iot_box ib WHERE emp_id ='21';
+UPDATE sidsa_service.iot_box SET emp_id=21 WHERE emp_id =18;
--- sidsa/sys_user.sql
+++ sidsa/sys_user.sql
@@ -1,4 +1,5 @@
 SELECT * FROM  sidsa_service.sys_user su;
 SELECT * FROM  sidsa_service.sys_user su WHERE nick_name  ='郑玉晶';
+SELECT * FROM  sidsa_service.sys_user su WHERE nick_name LIKE'薛%';
 SELECT * FROM  sidsa_service.sys_user su  WHERE role_id  = '4';
 SELECT * FROM  sidsa_service.sys_user su WHERE phone = '18513182830';
