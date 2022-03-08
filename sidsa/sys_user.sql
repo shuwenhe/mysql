5a7eabfcc4470e8f8022051b88b4d51609b13163
何书文（HSW01） <HSW01@SID.CN>
2022-03-08 17:48:35 +0800

    fix: where nick_name like '%hello%'

--- sidsa/sys_user.sql
+++ sidsa/sys_user.sql
@@ -1,5 +1,4 @@
 SELECT * FROM  sidsa_service.sys_user su;
-SELECT * FROM  sidsa_service.sys_user su WHERE nick_name  ='郑玉晶';
-SELECT * FROM  sidsa_service.sys_user su WHERE nick_name LIKE'薛%';
+SELECT * FROM  sidsa_service.sys_user su WHERE nick_name LIKE'%王%';
 SELECT * FROM  sidsa_service.sys_user su  WHERE role_id  = '4';
-SELECT * FROM  sidsa_service.sys_user su WHERE phone = '18513182830';
+SELECT * FROM  sidsa_service.sys_user su WHERE phone = '18513180856';
