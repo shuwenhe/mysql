56815cc5a28de7d9c22f337223a93c11e3e34f4a
何书文（HSW01） <HSW01@SID.CN>
2022-03-14 17:27:03 +0800

    fix: mysql

--- sidsa/workorders/get-work_order-problems.sql
+++ sidsa/workorders/get-work_order-problems.sql
@@ -1,1 +1,4 @@
+-- 127.0.0.1:8990/work_order/problems
 -- noAuth.GET("/work_order/problems", handler.ListWorkOrderProblem(logger, woSrv))
+-- shuwen-sidsa-service/interval/pkg/store/workorder/mysql.go:220
+SELECT * FROM `sid_work_order_problem;
