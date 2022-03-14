8cb5844aa0f77a7f6190364712b78cf2cbb959c1
何书文（HSW01） <HSW01@SID.CN>
2022-03-14 10:01:45 +0800

    fix: iot_box

--- sidsa/iot_box.sql
+++ sidsa/iot_box.sql
@@ -2,3 +2,5 @@ SELECT * FROM sidsa_service.iot_box ib;
 SELECT * FROM sidsa_service.iot_box ib WHERE emp_id ='18';
 SELECT * FROM sidsa_service.iot_box ib WHERE emp_id ='21';
 UPDATE sidsa_service.iot_box SET emp_id=21 WHERE emp_id =18;
+-- /workorders
+SELECT * FROM `iot_box` WHERE iot_box.id = 190304 ORDER BY `iot_box`.`id` LIMIT 1;
