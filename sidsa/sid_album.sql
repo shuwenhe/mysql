3614881afec64640ace60d89a2df2b7bb2fbf0ed
何书文（HSW01） <HSW01@SID.CN>
2022-03-15 13:41:20 +0800

    fix: sid_album.sql

--- sidsa/sid_album.sql
+++ sidsa/sid_album.sql
@@ -1,1 +1,2 @@
-SELECT * FROM  sidsa_service.sid_album ;
+SELECT * FROM  sidsa_service.sid_album;
+SELECT sid_album.*,count(sid_photo.id) as resource_count FROM `sid_album` LEFT JOIN sid_photo ON sid_photo.album_id = sid_album.id GROUP BY `sid_album`.`id` ORDER BY sid_album.photo_updated_at desc,sid_album.id desc LIMIT 20;
