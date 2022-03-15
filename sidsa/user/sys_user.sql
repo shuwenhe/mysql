66c6b72dbeae06c11eda5f73090dce2c8e225201
何书文（HSW01） <HSW01@SID.CN>
2022-03-15 13:58:13 +0800

    fix: iot & photo

---
+++ sidsa/user/sys_user.sql
@@ -0,0 +1,4 @@
+SELECT * FROM  sidsa_service.sys_user su;
+SELECT * FROM  sidsa_service.sys_user su WHERE nick_name LIKE'%王%';
+SELECT * FROM  sidsa_service.sys_user su  WHERE role_id  = '4';
+SELECT * FROM  sidsa_service.sys_user su WHERE phone = '18513180856';
