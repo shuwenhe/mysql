93d70d19384881df9c4c6fc9241435fc33309ce9
何书文（HSW01） <HSW01@SID.CN>
2022-03-17 10:57:44 +0800

    fix: mysql

---
+++ sidsa/iot/iot_user_project.sql
@@ -0,0 +1,1 @@
+SELECT * FROM sidsa_service.iot_user_project iup ;
---
+++ sidsa/iot/iot_variant.sql
@@ -0,0 +1,2 @@
+-- sidsa-service/internal/pkg/store/iot_variant.go:80
+UPDATE `iot_variant` SET `latest_report_time`=1647330075 WHERE imei = '240305005054406' AND var_id in (67,68,71,72,73,19,20,22);
