e944f3096232c58d80c162c6630a432cfdaab92f
何书文（HSW01） <HSW01@SID.CN>
2022-03-18 15:29:06 +0800

    fix: 219

--- sidsa/photo/sid_album.sql
+++ sidsa/photo/sid_album.sql
@@ -1,2 +1,3 @@
 SELECT * FROM  sidsa_service.sid_album;
+SELECT * FROM sidsa_service.sid_album sa WHERE name LIKE '%219%';
 SELECT sid_album.*,count(sid_photo.id) as resource_count FROM `sid_album` LEFT JOIN sid_photo ON sid_photo.album_id = sid_album.id GROUP BY `sid_album`.`id` ORDER BY sid_album.photo_updated_at desc,sid_album.id desc LIMIT 20;
