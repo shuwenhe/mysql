a7e3951b832bca0b9fcce43145c46e753154cd0c
何书文（HSW01） <HSW01@SID.CN>
2022-03-29 10:16:14 +0800

    feat: insert feedback

---
+++ sidsa/sid_feedback.sql
@@ -0,0 +1,1 @@
+INSERT INTO `sid_feedback` (`problem_desc`,`link`,`files`,`created_by`,`created_at`,`updated_at`,`deleted_at`) VALUES ('123','123',CAST('[]' AS JSON),484,'2022-03-29 10:07:15.22','2022-03-29 10:07:15.22',NULL);
