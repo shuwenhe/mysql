063d492951e34a22e65d48dd0890e73f99eceae6
何书文（HSW01） <HSW01@SID.CN>
2022-03-31 15:41:38 +0800

    feat: iot real data offline

--- sidsa/get-user-profile.sql
+++
@@ -1,5 +0,0 @@
--- m.GET("/user/profile", handler.ShowProfile(logger, svc))
--- shuwen-sidsa-service/interval/pkg/store/user/mysql.go:82
-SELECT user_id, email, phone, created_at, updated_at, phone, nick_name, password, avatar, sex,role_id,wx_openid FROM `sys_user` WHERE phone = '15010729356' ORDER BY `sys_user`.`user_id` LIMIT 1;
--- shuwen-sidsa-service/interval/pkg/store/sysrole/mysql.go:21
-SELECT * FROM `sys_role` WHERE role_id = 4 ORDER BY `sys_role`.`role_id` LIMIT 1;
--- sidsa/sys_user.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM  sidsa_service.sys_user su;
-SELECT * FROM  sidsa_service.sys_user su WHERE nick_name LIKE'%王%';
-SELECT * FROM  sidsa_service.sys_user su  WHERE role_id  = '4';
-SELECT * FROM  sidsa_service.sys_user su WHERE phone = '18513180856';
-SELECT sys_dept.dept_name,sys_user.nick_name,sys_user.user_id FROM sidsa_service.`sys_user` LEFT JOIN sidsa_service.`sys_dept` ON sys_dept.dept_id = sys_user.dept_id WHERE role_id = 4;
