76fd695abccaf2ba6253d8643c82c43a631a2ab8
何书文（HSW01） <HSW01@SID.CN>
2022-04-13 23:10:12 +0800

    feat:student & teacher

--- sidsa_service/iot_variant.sql
+++ sidsa_service/iot_variant.sql
@@ -1,2 +1,5 @@
+-- 285
+SELECT * FROM iot_variant iv WHERE imei = '240305005062304';
 -- sidsa-service/internal/pkg/store/iot_variant.go:80
 UPDATE `iot_variant` SET `latest_report_time`=1647330075 WHERE imei = '240305005054406' AND var_id in (67,68,71,72,73,19,20,22);
+
