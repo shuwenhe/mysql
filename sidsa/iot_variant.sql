fa24a99be2d12c3971ef2d425003af8d7256d160
shuwen <1201220707@pku.edu.cn>
2022-05-11 13:54:33 +0800

    feat: iot_variable

--- sidsa/iot_variant.sql
+++ sidsa/iot_variant.sql
@@ -3,3 +3,10 @@ SELECT * FROM iot_variant iv WHERE imei = '240305005062304';
 -- sidsa-service/internal/pkg/store/iot_variant.go:80
 UPDATE `iot_variant` SET `latest_report_time`=1647330075 WHERE imei = '240305005054406' AND var_id in (67,68,71,72,73,19,20,22);
 
+SELECT * FROM sidsa_service.iot_variant iv ;
+SELECT * FROM sidsa_service.iot_real_data order by time DESC ;
+
+select var_id ,name from sidsa_service.iot_variant  a
+        where imei = '240305005062304' and is_show =1
+        order by sort asc
+
--- sidsa/sys_user.sql
+++ sidsa/sys_user.sql
@@ -1,5 +1,7 @@
 SELECT * FROM  sidsa_service.sys_user su;
-SELECT * FROM  sidsa_service.sys_user su WHERE nick_name LIKE'%王%';
-SELECT * FROM  sidsa_service.sys_user su  WHERE role_id  = '4';
-SELECT * FROM  sidsa_service.sys_user su WHERE phone = '18513180856';
+SELECT * FROM  sidsa_service.sys_user su WHERE nick_name LIKE'%韩%';
+SELECT * FROM  sidsa_service.sys_user su  WHERE role_id  = '72';
+SELECT * FROM  sidsa_service.sys_user su WHERE phone = '15010729356';
 SELECT sys_dept.dept_name,sys_user.nick_name,sys_user.user_id FROM sidsa_service.`sys_user` LEFT JOIN sidsa_service.`sys_dept` ON sys_dept.dept_id = sys_user.dept_id WHERE role_id = 4;
+SELECT * FROM sidsa_service.`sys_dept` sd ;
+SELECT * FROM sidsa_service.sys_user WHERE (phone = '15010729356' and status = 2) AND `sys_user`.`deleted_at` IS NULL ORDER BY `sys_user`.`user_id` LIMIT 1
