753ca5f5c6afda82a1715d4516d6df3c8c96d309
shuwen <1201220707@pku.edu.cn>
2022-05-20 09:40:59 +0800

    fix: inner join

--- select.sql
+++ select.sql
@@ -1,7 +1,4 @@
-SELECT count(*) FROM `iot_user_project` WHERE user_id = 435 AND project_id = 290319 AND `iot_user_project`.`deleted_at` IS NULL;
-SELECT count(*) FROM `iot_user_project` WHERE user_id = 434 AND project_id = 190304 AND `iot_user_project`.`deleted_at` IS NULL;
 -- LIKE
-
 
 SELECT * 
 FROM sidsa_service.iot_alarm ia 
@@ -15,4 +12,5 @@ SELECT count(*) FROM `sid_photo`;
 -- GET \"/albums/13/photos\"
 SELECT sid_photo.*, DATE_FORMAT(created_at,'%Y-%m-%d') as date FROM sidsa_service.`sid_photo` WHERE album_id = 13 ORDER BY created_at desc LIMIT 30;
 
+SELECT count(*) FROM `iot_user_project` WHERE user_id = 435 AND project_id = 290319 AND `iot_user_project`.`deleted_at` IS NULL;
 
