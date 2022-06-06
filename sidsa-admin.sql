146726b3ddd181e5fa7c248880a5deefcc6c1c4a
ShuwenHe <1201220707@pku.edu.cn>
2022-06-06 14:34:06 +0800

    feat: problem

---
+++ sidsa-admin.sql
@@ -0,0 +1,8 @@
+-- /sidsa-admin/app/sidsa/service/sid_work_order_problem.go:74
+SELECT sqm.question_category_id,GROUP_CONCAT(ibt.box_type_name) AS box_type_name
+FROM sid_question_map sqm
+INNER JOIN iot_box_type ibt
+ON(ibt.box_type_id=sqm.box_type_id)
+WHERE sqm.question_category_id
+IN(1,2,3,4,5,6,7,8,9,10)
+GROUP BY sqm.question_category_id;
