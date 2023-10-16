8da7561928fd203081ab329329ccccb7a3a422f5
ShuwenHe <1201220707@pku.edu.cn>
2023-10-16 15:22:04 +0800

    feat: 495

--- sidsa/495.sql
+++ sidsa/495.sql
@@ -1,5 +1,5 @@
 -- 1-100 产量 
-SELECT id, imei, port_id, device_id, var_id, name, name_en, address_type, address_type2, data_type, `read_write`, note, unit, offset, offset2, is_linear, in_min, in_max, out_min, var_group, out_max, sort, linear_data_type, latest_report_time, deleted_at, is_show, var_type, expression, expression_args
+SELECT id, imei, port_id, device_id, var_id, name, name_en, address_type, address_type2, data_type, `read_write`, note, unit, offset, offset2, is_linear, in_min, in_max, out_min, var_group, out_max, sort, linear_data_type, latest_report_time, deleted_at, is_show, var_type, expression, expression_args, custom_group
 FROM sidsa_service.iot_variant
 WHERE imei = "240305005122847" AND name LIKE "%产量%";
 1 累计产量
