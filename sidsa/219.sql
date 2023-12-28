9e9ebd4fa8d56eda3835330f44b391bbf1bc8278
Shuwen He <1201220707@pku.edu.cn>
2023-12-28 09:45:57 +0800

    feat: 219

--- sidsa/219.sql
+++ sidsa/219.sql
@@ -1,13 +1,18 @@
+-- 更新数据
+SELECT * FROM sidsa_service.iot_variant WHERE imei = 240305005052836;
+UPDATE sidsa_service.iot_variant SET sort=9999 WHERE id=99287;
+UPDATE sidsa_service.iot_variant SET sort=9999 WHERE id>=99287 and id <= 99468;
+
+-- 查找变量名并排序sort
+SELECT id, imei, port_id, device_id, var_id, name, name_en, address_type, address_type2, data_type, `read_write`, note, unit, offset, offset2, is_linear, in_min, in_max, out_min, var_group, out_max, sort, linear_data_type, latest_report_time, deleted_at, is_show, var_type, expression, expression_args, custom_group
+FROM sidsa_service.iot_variant
+WHERE imei = 240305005052836 AND name LIKE '%混合器液压站油温过高%'
+
 -- PR219朔州金圆
 -- IMEI 240305005122847
 SELECT id, imei, port_id, device_id, var_id, name, name_en, address_type, address_type2, data_type, `read_write`, note, unit, offset, offset2, is_linear, in_min, in_max, out_min, var_group, out_max, sort, linear_data_type, latest_report_time, deleted_at, is_show, var_type, expression, expression_args
 FROM sidsa_service.iot_variant
 WHERE imei = "240305005052836" AND name = "混合器重量"
-
--- 更新数据
-SELECT * FROM sidsa_service.iot_variant WHERE imei = 240305005052836;
-UPDATE sidsa_service.iot_variant SET sort=9999 WHERE id=99287;
-UPDATE sidsa_service.iot_variant SET sort=9999 WHERE id>=99287 and id <= 99468;
 
 -- PR90219朔州金圆
 -- IMEI 240305005062444
