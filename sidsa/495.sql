3ca09f10ccfb4eaeac19685d6d07843ddd51bf98
Shuwen He <1201220707@pku.edu.cn>
2023-12-28 14:48:37 +0800

    feat: 495

--- sidsa/495.sql
+++ sidsa/495.sql
@@ -1,9 +1,9 @@
 -- 495
--- 240305005062748
+-- 240305005122847
 -- 更新数据
-SELECT * FROM sidsa_service.iot_variant WHERE imei = 240305005062748;
-UPDATE sidsa_service.iot_variant SET sort=9999 WHERE id=100156;
-UPDATE sidsa_service.iot_variant SET sort=9999 WHERE id>=100156 and id <= 100364;
+SELECT * FROM sidsa_service.iot_variant WHERE imei = 240305005122847;
+UPDATE sidsa_service.iot_variant SET sort=9999 WHERE id=99712;
+UPDATE sidsa_service.iot_variant SET sort=9999 WHERE id>=99712 and id <= 100015;
 
 -- 查找变量名并排序sort
 SELECT id, imei, port_id, device_id, var_id, name, name_en, address_type, address_type2, data_type, `read_write`, note, unit, offset, offset2, is_linear, in_min, in_max, out_min, var_group, out_max, sort, linear_data_type, latest_report_time, deleted_at, is_show, var_type, expression, expression_args, custom_group
