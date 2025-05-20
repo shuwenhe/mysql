721e98b4129e683d090dfd1b0e0f481c9b2842c3
Shuwen He <1201220707@pku.edu.cn>
2025-05-20 15:04:32 +0800

    fix:employee

--- employee/employee.sql
+++ employee/employee.sql
@@ -1,25 +1,6 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : shuwenmall
- Source Server Type    : MySQL
- Source Server Version : 50647
- Source Host           : 62.234.11.179:3306
- Source Schema         : shuwenmall
-
- Target Server Type    : MySQL
- Target Server Version : 50647
- File Encoding         : 65001
-
- Date: 27/03/2021 07:45:35
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
--- ----------------------------
--- Table structure for employee
--- ----------------------------
 DROP TABLE IF EXISTS `employee`;
 CREATE TABLE `employee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
